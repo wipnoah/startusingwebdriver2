25d171322f9196aa7b5a51922a3c5dbe018a0e05
Guillem Hernandez Sola <guillem@itnove.com>
2017-11-15 18:51:51 +0100

    Codi elemental selenium

--- src/test/java/com/itnove/trainings/junit/startUsingWebDriver/CheckBoxesTest.java
+++ src/test/java/com/itnove/trainings/junit/startUsingWebDriver/CheckBoxesTest.java
@@ -21,10 +21,12 @@ public class CheckBoxesTest extends BaseTest {
         //1
         driver.navigate().to("https://the-internet.herokuapp.com/checkboxes");
         //2
-        WebElement checkbox2 = driver.findElement(By.xpath(".//*[@id='checkboxes']/input[2]"));
+        WebElement checkbox2 =
+                driver.findElement(By.xpath(".//*[@id='checkboxes']/input[2]"));
         assertTrue(checkbox2.getAttribute("checked").equals("true"));
         //2.1
-        WebElement checkbox1 = driver.findElement(By.xpath(".//*[@id='checkboxes']/input[1]"));
+        WebElement checkbox1 =
+                driver.findElement(By.xpath(".//*[@id='checkboxes']/input[1]"));
         assertNull(checkbox1.getAttribute("checked"));
         //3
         checkbox1.click();
