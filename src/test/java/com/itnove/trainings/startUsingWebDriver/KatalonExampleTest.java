d8f8597165f5615d3859b72e017b68c17b9eb53f
wipnoah <wipnoah@gmail.com>
2018-04-04 14:49:53 +0200

    test committ

--- src/test/java/com/itnove/trainings/startUsingWebDriver/KatalonExampleTest.java
+++ src/test/java/com/itnove/trainings/startUsingWebDriver/KatalonExampleTest.java
@@ -12,30 +12,8 @@ import org.testng.annotations.Test;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-public class KatalonExampleTest {
-    private WebDriver driver;
-    private String browser;
+public class KatalonExampleTest extends BaseLocalRemoteWebDriverTest {
 
-    @BeforeClass(alwaysRun = true)
-    public void setUp() throws Exception {
-        browser = System.getProperty("browser");
-        if (browser.equalsIgnoreCase("chrome")) {
-            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-            System.setProperty("webdriver.chrome.driver",
-                    "src" + File.separator + "main"
-                            + File.separator + "resources"
-                            + File.separator + "chromedriver-linux");
-            driver = new ChromeDriver(capabilities);
-        } else {
-            DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-            System.setProperty("webdriver.gecko.driver",
-                    "src" + File.separator + "main"
-                            + File.separator + "resources"
-                            + File.separator + "geckodriver-linux");
-            driver = new FirefoxDriver(capabilities);
-        }
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
 
     @Test
     public void testUntitledTestCase() throws Exception {
@@ -47,8 +25,4 @@ public class KatalonExampleTest {
         driver.findElement(By.xpath("//div[@id='r1-0']/div/h2/a/b[2]")).click();
     }
 
-    @AfterClass(alwaysRun = true)
-    public void tearDown() throws Exception {
-        driver.quit();
-    }
 }
